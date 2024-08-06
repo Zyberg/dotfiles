dba5a0d49198b45bc5c1379d2b25357dc6f91a49
Zyberg <nikolajus.elkana@gmail.com>
2024-08-06 21:21:15 +0300

    Overall generalization and improvement to dotnet debugging setup; attach to existing dotnet process via a grep

--- init.lua
+++ init.lua
@@ -1,20 +1,30 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- TODO: move this out
+-- Utility function to get a pid of a process defined by a grep
+vim.g.get_pid = function(pattern)
+	local handle = io.popen("ps aux | grep " .. pattern .. " | awk '{print $2}' | head -n 1")
+	local result = handle:read("*a")
+	handle:close()
+
+	return result
+end
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,3 +1,68 @@
+vim.g.dotnet_build_project = function()
+	local default_path = vim.fn.getcwd() .. "/"
+	if vim.g["dotnet_last_proj_path"] ~= nil then
+		default_path = vim.g["dotnet_last_proj_path"]
+	end
+	local path = vim.fn.input("Path to your *proj file", default_path, "file")
+	vim.g["dotnet_last_proj_path"] = path
+	local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+	print("")
+	print("Cmd to execute: " .. cmd)
+	local f = os.execute(cmd)
+	if f == 0 then
+		print("\nBuild: ✔️ ")
+	else
+		print("\nBuild: ❌ (code: " .. f .. ")")
+	end
+end
+
+vim.g.dotnet_get_dll_path = function()
+	local request = function()
+		return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+	end
+
+	if vim.g["dotnet_last_dll_path"] == nil then
+		vim.g["dotnet_last_dll_path"] = request()
+	else
+		if
+			vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2)
+			== 1
+		then
+			vim.g["dotnet_last_dll_path"] = request()
+		end
+	end
+
+	return vim.g["dotnet_last_dll_path"]
+end
+
+vim.g.dotnet_get_pid = function()
+	local request = function()
+		local grep_expression = vim.fn.input("Process Name (grep expression): ")
+
+		vim.g["dotnet_last_pid_grep"] = grep_expression
+
+		return vim.g.get_pid(grep_expression)
+	end
+
+	if vim.g["dotnet_last_pid"] == nil then
+		vim.g["dotnet_last_pid"] = request()
+	else
+		local pid_of_grep = vim.g.get_pid(vim.g["dotnet_last_pid_grep"])
+
+		vim.fn.print("Currently the pid of '" .. vim.g["dotnet_last_pid_grep"] .. "' is: \t" .. pid_of_grep .. "\t")
+
+		if vim.fn.confirm("Do you want to change the pid?\n" .. vim.g["dotnet_last_pid"], "&yes\n&no", 2) == 1 then
+			if vim.fn.confirm("Do you want to change grep?", "&yes\n&no", 2) == 1 then
+				vim.g["dotnet_last_pid"] = request()
+			else
+				vim.g["dotnet_last_pid"] = vim.g.get_pid(vim.g["dotnet_last_pid_grep"])
+			end
+		end
+	end
+
+	return vim.g["dotnet_last_pid"]
+end
+
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
@@ -18,10 +83,21 @@ return {
 		dap.configurations.cs = {
 			{
 				type = "coreclr",
-				name = "launch - netcoredbg",
+				name = "Attach to Process",
+				request = "attach",
+				processId = function()
+					return vim.g.dotnet_get_pid()
+				end,
+			},
+			{
+				type = "coreclr",
+				name = "Launch - netcoredbg",
 				request = "launch",
 				program = function()
-					return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+					if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+						vim.g.dotnet_build_project()
+					end
+					return vim.g.dotnet_get_dll_path()
 				end,
 			},
 		}
@@ -51,5 +127,16 @@ return {
 		vim.keymap.set("n", "<leader>dj", dap.step_over, {})
 		vim.keymap.set("n", "<leader>dk", dap.step_into, {})
 		vim.keymap.set("n", "<leader>do", dap.step_out, {})
+
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = ">", texthl = "", linehl = "", numhl = "" })
+
+		-- TODO: eventually move this out to a dotnet config file
+		vim.api.nvim_set_keymap(
+			"n",
+			"<C-b>",
+			":lua vim.g.dotnet_build_project()<CR>",
+			{ noremap = true, silent = true }
+		)
 	end,
 }

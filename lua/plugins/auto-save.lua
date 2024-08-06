d0c7f2fc785a786dde40b5301c8f1991ff60b9a3
Zyberg <nikolajus.elkana@gmail.com>
2024-08-06 21:32:01 +0300

    Disable autosave, add auto brackets

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -10,14 +10,14 @@ return {
 				dim = 0.18, -- dim the color of `message`
 				cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
 			},
-			trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+			trigger_events = { }, -- "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
 			-- function that determines whether to save the current buffer or not
 			condition = function(buf)
 				local fn = vim.fn
 				local utils = require("auto-save.utils.data")
 
 				if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
-					return true -- met condition(s), can save
+					return false -- true -- met condition(s), can save
 				end
 				return false -- can't save
 			end,

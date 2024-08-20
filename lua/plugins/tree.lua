1b50337e413472764bef4082f938d66d8040432e
Zyberg <nikolajus.elkana@gmail.com>
2024-08-20 19:36:13 +0300

    Add toggle remap for nvim tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,10 +1,12 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    require("nvim-tree").setup({
-      update_focused_file = {
-        enable = true
-      }
-    })
-  end
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			update_focused_file = {
+				enable = true,
+			},
+		})
+
+		vim.api.nvim_set_keymap("n", "<M-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+	end,
 }

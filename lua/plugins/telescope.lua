58f012d04c452253426d5c1caaa4ec98bf96e1e0
Zyberg <nikolajus.elkana@gmail.com>
2024-08-21 00:45:24 +0300

    Add ability to grep files with args

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,48 @@
-return {{ 
-  "nvim-telescope/telescope.nvim", tag = "0.1.5",
-  dependencies = { "nvim-lua/plenary.nvim" },
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
 
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
-  end
-},
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        }
-      })
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+			vim.keymap.set(
+				"n",
+				"<leader>fg",
+				":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+				{}
+			)
+			vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = { "nvim-telescope/telescope-live-grep-args.nvim" },
+		config = function()
+			local lga_actions = require("telescope-live-grep-args.actions")
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+					["live_grep_args"] = {
+						auto_quoting = true, -- enable/disable auto-quoting
+						-- define mappings, e.g.
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+								-- freeze the current list and start a fuzzy search in the frozen list
+								["<C-space>"] = lga_actions.to_fuzzy_refine,
+							},
+						},
+					},
+				},
+			})
 
-      require("telescope").load_extension("ui-select")
-    end
-  }
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("live_grep_args")
+		end,
+	},
 }
